b564d288c6ade43d47b17e781ee5583207875e77
Rafael França <rafaelfranca@hotmail.com.br>
2020-09-27 12:33:51 -0300

    Configure Models

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column('timestamp with time zone')
   created_at: Date;
 
+  @Column('timestamp with time zone')
   updated_at: Date;
 }
 
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,31 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column('timestamp with time zone')
   created_at: Date;
 
+  @Column('timestamp with time zone')
   updated_at: Date;
 }
 
