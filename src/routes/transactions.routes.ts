cf36ced485623af994579c254e41e2de432b2459
Rafael França <rafaelfranca@hotmail.com.br>
2020-09-27 12:37:51 -0300

    Solution: Import Transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import uploadConfig from '../config/upload';
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
@@ -44,8 +49,12 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
+transactionsRouter.post('/import', upload.single('file'), async (request, response) => {
+  const importTransactions = new ImportTransactionsService;
+
+  const transactions = await importTransactions.execute(request.file.path);
+
+  return response.json(transactions);
 });
 
 export default transactionsRouter;
